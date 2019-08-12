93e9466143fac974b1921ade555364666f8ac174
ajinra020307 <ajinra020307@gmail.com>
2019-08-12 15:12:38 +0530

    all

--- js/common.js
+++ js/common.js
@@ -3,9 +3,6 @@
 document.querySelector('.hamburger').addEventListener('click', displaymenu)
 
 function displaymenu(e){
-
-     	  document.querySelector('.menu2container').style.display="flex"
+   document.querySelector('.menu2container').style.display="flex"
  
-
-
 }
